b26ecef09fd628865c7d364ab14faba915af0390
ravihuang <ravi.huang@gmail.com>
2016-01-26 13:02:09 +0800

    Update start.sh

--- conf/start.sh
+++ conf/start.sh
@@ -7,8 +7,8 @@ STATUS=`grep $ip /etc/dnsmasq.conf`
 if [ $? -eq 1 ]; then
    echo "Set DHCP IP/Range:"
    prefix=${ip%.*}
-   sed -i "s/192.168.103.11/$ip/g" /etc/dnsmasq.conf
-   sed -i "s/192.168.103/$prefix/g"  /etc/dnsmasq.conf
+   sed -i "s/localaddr/$ip/g" /etc/dnsmasq.conf
+   sed -i "s/netaddr/$prefix/g"  /etc/dnsmasq.conf
 fi
 
 echo "$ip pxe.ravihuang.net" >> /etc/hosts
